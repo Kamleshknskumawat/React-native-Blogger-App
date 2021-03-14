f42f2de7340528820b22af1984e466cc228340d3
WJAVA Kamlesh K. Kumawat <kamleshkk@webline.local>
2021-03-14 18:13:55 +0530

    madal add

--- src/screens/MainScreen/ProfileScreen.js
+++ src/screens/MainScreen/ProfileScreen.js
@@ -1,12 +1,22 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet, FlatList, ActivityIndicator, Image, ScrollView, SafeAreaView } from 'react-native';
+import { View, StyleSheet, FlatList, ActivityIndicator, Image, ScrollView, SafeAreaView } from 'react-native';
 import { Header, Item, Input, Icon, Button, Container, Subtitle } from 'native-base';
-import { Left, Body, Right, Title } from 'native-base';
+import { Content, List, ListItem, Left, Body, Right, Thumbnail, Text, Textarea, Form } from 'native-base';
+import { Title } from 'native-base';
 import { useNavigation, useRoute } from '@react-navigation/native';
+import { Linking } from 'react-native';
+import { sendEmail } from './send-email';
+import Modal from 'react-native-modal';
 const ProfileScreen = () => {
   const [datas, setDatas] = useState();
   const [isEndLoading, setIsEndLoading] = useState(false);
   const navigation = useNavigation();
+  const [isModalVisible, setModalVisible] = useState(false);
+  const [emailBody, setEmailBody] = useState('');
+  const [time, setTime] = useState('');
+  const toggleModal = () => {
+    setModalVisible(!isModalVisible);
+  };
   getData = async () => {
     const apiURL = "https://jsonplaceholder.typicode.com/photos?limit=10&_page=1";
 
@@ -45,40 +55,32 @@ const ProfileScreen = () => {
         </View> : null
     );
   }
-  const handleLoadMore = () => {
-    console.log("handleLoadMore {} called");
-    console.log("current pages ");
-    // Toast.show({
-    //   text: 'Please take breath...',
-    //   buttonText: 'Okay'
-    // })
-    // setPages(pages + 10);
-    // console.log("current new pages " + pages);
-    // setIsEndLoading(true);
-    // listOfPost(pages)
-
+  const sendEmailUs = () => {
+    sendEmail(
+      'kamleshknskumawat@gmail.com',
+      'Bug and Report!',
+      emailBody
+    ).then(() => {
+      console.log('Our email successful provided to device mail ');
+    });
+    toggleModal();
   }
+
+  const [dt, setDt] = useState(new Date().toLocaleString());
+
+  useEffect(() => {
+      let secTimer = setInterval( () => {
+        setDt(new Date().toLocaleString())
+      },1000)
+  
+      return () => clearInterval(secTimer);
+  }, []);
+ 
+
   return (
-    // <Container>
-    //   <SafeAreaView style={styles.container}>
-    //     <View style={styles.container}>
-
-    //       <FlatList
-    //         data={datas}
-    //         renderItem={listss}
-    //         keyExtractor={(item, index) => index.toString()}
-    //         onEndReachedThreshold={0.01}
-    //         ListFooterComponent={renderFooter}
-    //         onEndReached={handleLoadMore}
-    //         onEndReachedThreshold={0.1}
-    //       />
-
-    //     </View>
-    //   </SafeAreaView >
-    // </Container>
     <Container>
       <Header>
-        <Left>       
+        <Left>
           <Button transparent onPress={() => navigation.goBack()}>
             <Icon name='arrow-back' />
           </Button>
@@ -89,7 +91,44 @@ const ProfileScreen = () => {
         </Body>
         <Right />
       </Header>
+      <Content>
+
+        <View style={{ flex: 1 }}>
+          <Modal isVisible={isModalVisible}>
+            <View style={{ flex: 1, marginTop: 150 }}>
+
+              <Form>
+                <Textarea style={{ backgroundColor: 'white' }} rowSpan={5} bordered placeholder="Please write your bug for help us." onChangeText={setEmailBody} />
+              </Form>
+
+              <View style={{ flex: 1, flexDirection: 'row', paddingLeft: 120 }}>
+                <Button onPress={toggleModal} ><Text>Hide modal</Text></Button>
+                <Button onPress={sendEmailUs} ><Text>Send Email</Text></Button>
+              </View>
+            </View>
+          </Modal>
+        </View>
+        <List>
+
+          <ListItem avatar onPress={toggleModal}>
+            <Left>
+              <Thumbnail source={{ uri: 'https://avatars.githubusercontent.com/u/11689830?s=460&u=05d5ec08b7df42ee9f7d45973e560c23da309830&v=4' }} />
+            </Left>
+            <Body>
+              <Text>Kamlesh Kumawat : Developer</Text>
+              <Text note>{dt}</Text>
+              <Text>Tap & Write</Text>
+              
+            </Body>
+            <Right >
+             
+              <Text note>Online</Text>
+            </Right>
+          </ListItem>
+        </List>
+      </Content>
     </Container>
+
   );
 };
 
